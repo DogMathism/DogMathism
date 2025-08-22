3d2916b534fe90fcab4b7779d2cb9c66ff6024d8
DogMathism <artyomarutynyan0127@gmail.com>
2025-08-22 03:36:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -324,7 +324,7 @@ def main():
         entry_points=[CommandHandler("start", start)],
         states={ASK_PHONE: [MessageHandler(filters.CONTACT, phone_received)]},
         fallbacks=[CommandHandler("cancel", cancel)],
-        per_message=False
+        per_message=True
     )
     app.add_handler(conv_handler)
 
