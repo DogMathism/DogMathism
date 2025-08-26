d8613af99be450adc72148ddc3ed531c50b1ea5d
DogMathism <artyomarutynyan0127@gmail.com>
2025-08-26 20:14:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -377,6 +377,7 @@ async def finalize_and_materials(update: Update, context: ContextTypes.DEFAULT_T
             await reply(
                 update,
                 f"❌ Для получения материалов подпишитесь на канал {CHANNELS_BY_SUBJECT[subject]} и попробуйте снова."
+                reply_markup=reply_markup
             )
             # Покажем кнопку выбора роли, чтобы юзер мог пойти в другой сценарий
             await return_to_role_selection(update)
